22aa54ed25229da8aa32653d561a1377440abe75
JBuzz826 <59944676+JBuzz826@users.noreply.github.com>
2024-06-06 21:37:10 -0600

    OAUTH working

--- app.js
+++ app.js
@@ -1,20 +1,42 @@
-require("dotenv").config();
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
 const cors = require('cors');
+const { auth } = require('express-openid-connect');
 
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(bodyParser.json()).use(cors()).use('/', require('./routes'));
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET, // Store secrets in environment variables
+  baseURL: process.env.BASE_URL || 'http://localhost:8080',
+  clientID: process.env.CLIENT_ID, // Store sensitive data in environment variables
+  issuerBaseURL: process.env.ISSUER_BASE_URL // Store sensitive data in environment variables
+};
 
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
 
-mongodb.initDb((err, mongodb) => {
+app.use(bodyParser.json());
+app.use(cors());
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+// Routes
+app.use('/pilots', require('./routes/pilots'));
+
+mongodb.initDb((err) => {
   if (err) {
     console.log(err);
   } else {
-    app.listen(port);
-    console.log(`Connected to DB and listening on ${port}`);
+    app.listen(port, () => {
+      console.log(`Connected to DB and listening on ${port}`);
+    });
   }
 });
--- routes/pilots.js
+++ routes/pilots.js
@@ -1,18 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const { requiresAuth } = require('express-openid-connect');
 
-//Creates the pilots controller needed to edit pilot data
+// Creates the pilots controller needed to edit pilot data
 const pilotsController = require('../controllers/pilots');
 
-
-router.get('/', pilotsController.getAll);
-
-router.get('/:id', pilotsController.getSingle);
-
-router.post('/', pilotsController.createPilot);
-
-router.put('/:id', pilotsController.updatePilot);
-
-router.delete('/:id', pilotsController.deletePilot);
+router.get('/', requiresAuth(), pilotsController.getAll);
+router.get('/:id', requiresAuth(), pilotsController.getSingle);
+router.post('/', requiresAuth(), pilotsController.createPilot);
+router.put('/:id', requiresAuth(), pilotsController.updatePilot);
+router.delete('/:id', requiresAuth(), pilotsController.deletePilot);
 
 module.exports = router;
